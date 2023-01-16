20881d47acb03ea347081eb7a982a31486c648ce
chuckhalliday <charleshallidayclarkii@gmail.com>
2023-01-15 18:44:14 -0800

    Additional form fields

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,17 +14,35 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          My Permissions
+          My Desires
         </h1>
-        <form>
+        <form className={styles.permissionForm}>
+          <label>Username / Phone Number / Email</label>
+          <label htmlFor="any">Any:</label>
+          <input type="radio" id="any" name="user"/>
+          <label htmlFor="any">Any Except:</label>
+          <input type="radio" id="except" name="user"/>
+          <label htmlFor="any">Only:</label>
+          <input type="radio" id="only" name="user"/>
           <textarea className={styles.formContent}></textarea>
+          <label>Category</label>
           <input list="category" className={styles.formContent}/>
           <datalist id="category">
-            <option value="activity"/>
-            <option value="business"/>
-            <option value="dating"  />
+            <option value="Activity"/>
+            <option value="Food"/>
+            <option value="Business"/>
+            <option value="Dating"/>
           </datalist>
+          <label>Desire</label>
           <textarea className={styles.formContent}></textarea>
+          <label>Date</label>
+          <label htmlFor="any">Any:</label>
+          <input type="radio" id="any" name="date"/>
+          <label htmlFor="any">Any Except:</label>
+          <input type="radio" id="except" name="date"/>
+          <label htmlFor="any">Only:</label>
+          <input type="radio" id="only" name="date"/>
+          <input className={styles.formContent} type="date" />
           <button className={styles.formButton}>Consent</button>
         </form>
         <ul className={styles.permissions}>
